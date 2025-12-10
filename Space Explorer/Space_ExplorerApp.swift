aa31380c14823dea42cc8a0ee6f77edcb0e7c3ac
Novus99 <novusrepo@gmail.com>
2025-12-10 18:55:40 +0100

    Initial Commit

---
+++ Space Explorer/Space_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_ExplorerApp.swift
+//  Space Explorer
+//
+//  Created by Novus on 10/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Space_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
